175411e0fbcf7cb19519a5c01e7de22805791ea0
rsakie <rsakie@yahoo.com>
2017-07-13 13:04:37 -0400

    added a boat class and created a boat object

---
+++ Boat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceandPolymorphism
+{
+    class Boat : Vehicle
+    {
+        private double waterDrag;
+
+        public Boat(int seats, int carryingCapacity, string color, double movementSpeed, double waterDrag)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+            this.waterDrag = waterDrag;
+        }
+
+        public override void Move()
+        {
+            distanceTraveled += movementSpeed * waterDrag;
+            // += --- distaceTraveled = distance + movementSpeed * waterdrag
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace InheritanceandPolymorphism
     {
         static void Main(string[] args)
         {
+            Boat boat1 = new Boat(2, 500, "White and Yellow", 70.0d, .78d);
+            boat1.Move();
+            boat1.Move();
+            Console.WriteLine("Boat" + boat1.GetDistanceTraveled());
         }
     }
 }
